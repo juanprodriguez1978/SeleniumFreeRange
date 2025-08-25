700443737301982d4f7e6c52fae706dcd68bc926
JUANP <juanprodriguez.mail@gmail.com>
2025-08-25 08:56:06 +0200

    Update 1 test ok modify again 2

--- src/test/java/steps/FreeRangeSteps.java
+++ src/test/java/steps/FreeRangeSteps.java
@@ -49,7 +49,7 @@ public class FreeRangeSteps extends BasePage {
     public void validateCheckoutPlans() {
         /*List<String> lista = registro.returnPlanDropdownValues();
         List<String> listaEsperada = Arrays.asList("Academia: $16.99 / mes • 17 productos",
-                  "Academia: $176 / año • 17 productos", "Free: Gratis • 1 producto");
+                 "Academia: $176 / año • 17 productos", "Free: Gratis • 1 producto");
         Assert.assertEquals(listaEsperada, lista);*/
     }
 
